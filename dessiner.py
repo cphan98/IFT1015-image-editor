3e30fa94a9d363c270baa1347fc53f43684cdeec
Cynthia <cynthiaphan98@gmail.com>
2022-11-14 17:24:24 -0500

    Edit traiterProchainClic

--- dessiner.py
+++ dessiner.py
@@ -225,6 +225,7 @@ def traiterProchainClic(boutons):
         else:
             if position.x > 0 and position.x < getScreenWidth() and position.y > hauteurMenu and position.y < getScreenHeight():
                 debut = position
+                couleur = couleurRectangle
                 dessinerRectangleFlottant(imageOriginale, debut, couleur)
             else:
                 bouton = trouverBouton(boutons, position)
