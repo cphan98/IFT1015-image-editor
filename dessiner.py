a9f058f044c4d5348e0ec7147b714f60c1dfa9bc
Cynthia <cynthiaphan98@gmail.com>
2022-11-14 22:10:48 -0500

    Edit dessinerRectangleFlottant

--- dessiner.py
+++ dessiner.py
@@ -178,7 +178,6 @@ def dessinerRectangleFlottant(imageOriginale, debut, couleur):
             coin2 = struct(x=max(debut.x, souris.x), y=max(debut.y, souris.y))
             rectangle = struct(coin1=struct(x=debut.x, y=debut.y),
                                coin2=struct(x=souris.x, y=souris.y))
-
             restaurerImage(imageOriginale, rectangle)
             fillRectangle(coin1.x, coin1.y, coin2.x - coin1.x,
                           coin2.y - coin1.y, couleurRectangle)
